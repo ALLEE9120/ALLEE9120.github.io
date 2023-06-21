37c63c23c044136c77aa6d5ea979e2aa6a9d34a4
Muhammad Ali <791600ali@gmail.com>
2023-06-21 18:31:26 +1000

    pt4

--- Dev-Folio/src/Components/Projects.jsx
+++ Dev-Folio/src/Components/Projects.jsx
@@ -37,6 +37,10 @@ export default function Projects({color, bgColor}) {
         <div className="skills-div" style={skillDivStyle}>
           <div className="project">
             <h1 className="frontend-title">TripItEasy</h1>
+            <div className="frontend-list">
+              <img src="https://i.imgur.com/5Z3ZQ8X.png" alt="TripItEasy" />
+              <p>Travel App that allows users to create trips and add activities to their trips. Users can also view other users trips and activities.</p>
+              </div>
           </div>
         </div>
         <div className="tools-title bottom">
