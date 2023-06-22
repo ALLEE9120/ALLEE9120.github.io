8e86c6c72642bae05cce1c80ace9995b0da18b13
Muhammad Ali <791600ali@gmail.com>
2023-06-22 18:00:48 +1000

    contact form

--- Dev-Folio/src/Components/Contact.jsx
+++ Dev-Folio/src/Components/Contact.jsx
@@ -1,8 +1,11 @@
 
 
 export default function Contact({color, bgColor}) {
+  const divStyle = {
+    color: color,
+  }
   return (
-    <div className="contact-div">
+    <div className="contact-div" style={divStyle}>
       <div className="contact-body">
         <div className="contact-title top">
           <h1>&lt;div id='contact'&gt;</h1>
