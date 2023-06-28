4723bce4a39460837037c99b0e336dfb90932931
Muhammad Ali <791600ali@gmail.com>
2023-06-28 13:28:50 +1000

    contact

--- Dev-Folio/src/Components/Contact.jsx
+++ Dev-Folio/src/Components/Contact.jsx
@@ -3,7 +3,7 @@
 import PropTypes from 'prop-types'
 import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
-export default function Contact({color, bgColor}) {
+export default function Contact({color}) {
   const divStyle = {
     color: color,
   }
@@ -15,8 +15,56 @@ export default function Contact({color, bgColor}) {
 
   const inputStyle = {
     border: `1px solid ${color}`,
-    backgroundColor: bgColor,
+    backgroundColor: 'transparent',
+    zIndex: '10',
+    width: '100%',
   }
+
+  const spanStyle = {
+    color: color,
+    marginLeft: '12px',
+    fontSize: '1.2rem',
+    fontWeight: '500',
+    letterSpacing: '1px',
+    position: 'absolute',
+    top: '8px',
+    zIndex: '0',
+    transition: 'all 0.1s ease-in-out',
+  }
+
+  const divStyle2 = {
+    position: 'relative',
+    width: '100%',
+  }
+
+  const handleSpanRemoval = (e) => {
+    const span = e.target.previousSibling
+    if (e.target.value !== '') {
+      span.style.opacity = '0'
+    } else {
+      span.style.opacity = '1'
+    }
+  }
+
+  const handleSpanCLick = (e) => {
+    const span = e.target
+    const input = span.nextSibling
+    input.focus()
+  }
+
+  const submitStyle = {
+    border: `1px solid ${color}`,
+    backgroundColor: 'transparent',
+    zIndex: '10',
+    width: '100%',
+    marginTop: '20px',
+    padding: '10px',
+    cursor: 'pointer',
+    transition: 'all 0.1s ease-in-out',
+  }
+
+  const array = ['Name', 'Email']
+
 
 
   return (
@@ -36,10 +84,21 @@ export default function Contact({color, bgColor}) {
             </div>
           </div>
           <form className='contact-form' style={formStyle}>
-            <input type="text" id="name" name="name" placeholder='Name' style={inputStyle}></input>
-            <input type="text" id="email" name="email" placeholder='Email' style={inputStyle}></input>
-            <textarea type="text" id="message" name="message" placeholder='Message' style={inputStyle}></textarea>
-            <input type="submit" className='submit' value="Send"></input>
+            {array.map((item, index) => {
+              return (
+                <div style={divStyle2} key={index}>
+                  <span style={spanStyle} onClick={handleSpanCLick}>{item}</span>
+                  <input type="text" id={item} name={item} style={inputStyle} onChange={handleSpanRemoval}></input>
+                </div>
+              )
+            })}
+            <div style={divStyle2}>
+              <span style={spanStyle} onClick={handleSpanCLick}>Message</span>
+              <textarea type="text" id='message' name='message' style={inputStyle} onChange={handleSpanRemoval}></textarea>
+              </div>
+            <div style={divStyle2}>
+              <input type="submit" value="Submit" style={submitStyle}></input>
+            </div>
           </form>
         </div>
         <div className="contact-title bottom">
