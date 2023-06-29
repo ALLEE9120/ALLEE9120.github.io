2a6fd5b1d732cd9f41fc9c33a135a8dc6b1b909e
Muhammad Ali <791600ali@gmail.com>
2023-06-29 15:44:14 +1000

    refactoring 2

--- Dev-Folio/src/Components/ProjectComponent.jsx
+++ Dev-Folio/src/Components/ProjectComponent.jsx
@@ -24,27 +24,21 @@ export default function ProjectComponent({color, title, description, techStack, 
 
 
   return (
-    <div>
-      <div className="allinfo-div" style={skillDivStyle}>
-        <div className="project">
-          <div className="project-list">
-            <h1 className="project-title">{title}</h1>
-            <p style={{color: color}}>{description}</p>
-            <div className="project-stack">
-              <h2 className='gray'>// Tech Stack</h2>
-              <ul>
-                {techStack.map((tech, index) => {
-                  return <li key={index} style={{color: bgColor, backgroundColor: color}}>{tech}</li>
-                })}
-              </ul>
-            </div>
-            <h2 className='gray'>// See it in action</h2>
-            <div className="project-links">
-              <a onMouseOver={handleLinkHover} onMouseOut={handleLinkUnhover} style={{color: hover === 'Demo' ? '#3182CE' : color}} className={'Demo'} href="https://tripiteasy.herokuapp.com/">Visit Site <FaLink style={{marginLeft: '20px'}}/></a>
-              <a onMouseOver={handleLinkHover} onMouseOut={handleLinkUnhover} style={{color: hover === 'Code' ? '#3182CE' : color}} className={'Code'} href="https://github.com/chiara-muller/TripItEasy">See the code<FaGithub style={{marginLeft: '20px'}}/></a>
-            </div>
-          </div>
-        </div>
+    <div className="allinfo-div" style={skillDivStyle}>
+      <h1 className="project-title">{title}</h1>
+      <p style={{color: color}}>{description}</p>
+      <div className="project-stack">
+        <h2 className='gray'>// Tech Stack</h2>
+        <ul>
+          {techStack.map((tech, index) => {
+            return <li key={index} style={{color: bgColor, backgroundColor: color}}>{tech}</li>
+          })}
+        </ul>
+      </div>
+      <h2 className='gray'>// See it in action</h2>
+       <div className="project-links">
+        <a onMouseOver={handleLinkHover} onMouseOut={handleLinkUnhover} style={{color: hover === 'Demo' ? '#3182CE' : color}} className={'Demo'} href="https://tripiteasy.herokuapp.com/">Visit Site <FaLink style={{marginLeft: '20px'}}/></a>
+        <a onMouseOver={handleLinkHover} onMouseOut={handleLinkUnhover} style={{color: hover === 'Code' ? '#3182CE' : color}} className={'Code'} href="https://github.com/chiara-muller/TripItEasy">See the code<FaGithub style={{marginLeft: '20px'}}/></a>
       </div>
     </div>
   )
--- Dev-Folio/src/Components/Projects.jsx
+++ Dev-Folio/src/Components/Projects.jsx
@@ -15,44 +15,41 @@ export default function Projects({color, bgColor}) {
 
   return (
     <div className="projects-div" style={toolDivStyle}>
-      <div className="projects-body" style={bodyStyle}>
-        <div className="projects-title top">
-          <h1>&lt;div id='projects'&gt;</h1>
-        </div>
-        <ProjectComponent
-          color={color}
-          title={'TripItEasy'}
-          description={'Mobile travel web app that simplifies users experience to create trips and add activities to their trips.'}
-          bgColor={bgColor}
-          techStack={[
-          'Ruby on Rails',
-          'Javascript',
-          'BootStrap',
-          'SCSS',
-          'SQLite',
-          'Google Maps API',
-          'Heroku'
-        ]
-        }/>
-
-        <ProjectComponent
-          color={color}
-          title={'TripItEasy'}
-          description={'Mobile travel web app that simplifies users experience to create trips and add activities to their trips.'}
-          bgColor={bgColor}
-          techStack={[
-          'Ruby on Rails',
-          'Javascript',
-          'BootStrap',
-          'SCSS',
-          'SQLite',
-          'Google Maps API',
-          'Heroku'
-        ]
-        }/>
-        <div className="projects-title bottom">
-          <h1>&lt;/div&gt;</h1>
-        </div>
+      <div className="projects-title top">
+        <h1>&lt;div id='projects'&gt;</h1>
+      </div>
+      <ProjectComponent
+        color={color}
+        title={'TripItEasy'}
+        description={'Mobile travel web app that simplifies users experience to create trips and add activities to their trips.'}
+        bgColor={bgColor}
+        techStack={[
+        'Ruby on Rails',
+        'Javascript',
+        'BootStrap',
+        'SCSS',
+        'SQLite',
+        'Google Maps API',
+        'Heroku'
+        ]}
+      />
+      <ProjectComponent
+        color={color}
+        title={'TripItEasy'}
+        description={'Mobile travel web app that simplifies users experience to create trips and add activities to their trips.'}
+        bgColor={bgColor}
+        techStack={[
+        'Ruby on Rails',
+        'Javascript',
+        'BootStrap',
+        'SCSS',
+        'SQLite',
+        'Google Maps API',
+        'Heroku'
+        ]}
+      />
+      <div className="projects-title bottom">
+        <h1>&lt;/div&gt;</h1>
       </div>
     </div>
   )
