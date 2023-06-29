634896de42414cd70fc3db58af11fd68d6e55c9f
Muhammad Ali <791600ali@gmail.com>
2023-06-29 16:51:09 +1000

    performance optimization

--- Dev-Folio/src/main.jsx
+++ Dev-Folio/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './parts/Home.jsx'
-import './app.scss'
+import './app.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- Dev-Folio/src/parts/Home.jsx
+++ Dev-Folio/src/parts/Home.jsx
@@ -2,14 +2,26 @@ import { useState, useEffect } from 'react'
 import Background from "../Components/Background"
 import HomePage from "../Components/HomePage"
 import NavBar from "../Components/NavBar"
-import ToolsPage from "../Components/ToolsPage"
-import Projects from "../Components/Projects"
-import Contact from "../Components/Contact"
+import { lazy, Suspense } from 'react';
 
 export default function Home() {
   let [lightMode, setLightMode] = useState(false);
   let [bgColor, setBgColor] = useState('#010710');
   let [color, setColor] = useState('#ffffff');
+
+  const ToolsPage = lazy(() => import('../Components/ToolsPage'));
+  const Projects = lazy(() => import('../Components/Projects'));
+  const Contact = lazy(() => import('../Components/Contact'));
+
+  const LoadStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    backgroundColor: bgColor,
+    color: color,
+    fontSize: '30px',
+  }
 
   function handleChangeMode() {
     setLightMode(!lightMode);
@@ -27,12 +39,14 @@ export default function Home() {
 
   return (
     <div>
-      <NavBar lightMode={lightMode} onHandleChangeMode={handleChangeMode} bgColor={bgColor} color={color} />
-      <Background bgColor={bgColor} color={color}/>
-      <HomePage bgColor={bgColor} color={color}/>
-      <ToolsPage bgColor={bgColor} color={color}/>
-      <Projects bgColor={bgColor} color={color}/>
-      <Contact bgColor={bgColor} color={color}/>
+      <Suspense fallback={<div style={LoadStyle}>Loading...</div>}>
+        <NavBar lightMode={lightMode} onHandleChangeMode={handleChangeMode} bgColor={bgColor} color={color} />
+        <Background bgColor={bgColor} color={color}/>
+        <HomePage bgColor={bgColor} color={color}/>
+        <ToolsPage bgColor={bgColor} color={color}/>
+        <Projects bgColor={bgColor} color={color}/>
+        <Contact bgColor={bgColor} color={color}/>
+      </Suspense>
     </div>
   )
 }
