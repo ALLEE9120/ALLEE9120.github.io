d2e3aee718e1bc4219f50b12b13f95a6deae6f62
Muhammad Ali <791600ali@gmail.com>
2023-07-05 15:05:48 +1000

    grrrr

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   //https://github.com/ALLEE9120/ALLEE9120.github.io
-  base: '/ALLEE9120.github.io/',
+  base: '/',
   plugins: [react()],
 })
